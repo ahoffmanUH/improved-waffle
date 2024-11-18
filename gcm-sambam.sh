bd1b7daf90ffec5ecedd4e344fc0f9df543b4499
ahoffmanUH <ahoffman@cougarnet.uh.edu>
2024-11-18 07:43:03 -0600

    updated alignment and trimming

--- gcm-sambam.sh
+++ gcm-sambam.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=4 # requests 4 threads/cpus per task. You can also set ntasks, but by default it is 1
 #SBATCH --mem=10Gb # specify the amount of memory to request for the job; in theory HISAT2 should require only 6.5 Gb
-#SBATCH --output=sambam-alignment3.out #output file name
+#SBATCH --output=cf-copd-alignment.out #output file name
 
-/project/stuckert/ahoffman/samtools-1.3.1/samtools view -bS $(pwd)/alignment3.sam > $(pwd)/alignment3.bam
+/project/stuckert/ahoffman/samtools-1.3.1/samtools view -bS $(pwd)/alignment-NEW.sam > $(pwd)/alignment-NEW.bam
 
 #convert bam file to sorted bam
-/project/stuckert/ahoffman/samtools-1.3.1/samtools sort alignment3.bam -o alignment3sorted.bam
+/project/stuckert/ahoffman/samtools-1.3.1/samtools sort alignment-NEW.bam -o sorted-newAlignment.bam
+
+#move to Stringtie
+stringtie -G /project/stuckert/ahoffman/refgenome/Homo_sapiens.GRCh38.113.gtf -o cf-copd-aligned.gtf sorted-newAlignment.bam 
