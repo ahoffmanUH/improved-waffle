62bc8c46821fd92c0a67ab336f6529d08032dc3d
ahoffmanUH <ahoffman@cougarnet.uh.edu>
2025-02-10 16:53:09 -0600

    Update gcm-trimming.sh

--- gcm-trimming.sh
+++ gcm-trimming.sh
@@ -6,6 +6,7 @@
 
 #Use trimmomatic to remove Illumina Nextera adaptor sequences
 #first samples
+#the following is sample code used as a practice from class. The same pipeline was applied to future data downloads and updates
 java -jar /project/stuckert/ahoffman/Trimmomatic-0.39/trimmomatic-0.39.jar PE -phred33 -threads 5 -baseout SRR24750967_TRIM.fastq SRR24750967_1.fastq SRR24750967_2.fastq LEADING:3 TRAILING:3 ILLUMINACLIP:/project/stuckert/bioinformatics/lab7/barcodes.fa:2:30:10:8:TRUE MINLEN:25
 
 java -jar /project/stuckert/ahoffman/Trimmomatic-0.39/trimmomatic-0.39.jar PE -phred33 -threads 5 -baseout SRR7905008_TRIM.fastq SRR7905008_1.fastq SRR7905008_2.fastq LEADING:3 TRAILING:3 ILLUMINACLIP:/project/stuckert/bioinformatics/lab7/barcodes.fa:2:30:10:8:TRUE MINLEN:25
