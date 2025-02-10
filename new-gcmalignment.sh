909b1f51cb21cda7a093a742d09700364f974185
ahoffmanUH <ahoffman@cougarnet.uh.edu>
2025-02-10 16:54:51 -0600

    Update new-gcmalignment.sh

--- new-gcmalignment.sh
+++ new-gcmalignment.sh
@@ -9,5 +9,7 @@
 #align paired end reads to refgenome .ht2 files in 'genomes'
 #phred33= standard Illumina quality score is in Phred33 format
 #summary stats needed to assess alignment quality
+
+#sample code from class project in case it is needed for future data downloads and trimming
 #use default parameters for mismatch (mx = 6, min = 2), read gap (5,3), and reference gap (5,3)
 /project/stuckert/ahoffman/hisat2/hisat2 --phred33 --dta -x /project/stuckert/ahoffman/refgenome/genomes/genome -1 $(pwd)/SRR7905008_TRIM_1P.fastq -2 $(pwd)/SRR7905008_TRIM_2P.fastq -S alignment3.sam --summary-file summarystatsFinal.txt
